035b20910b73ea704d34057cdecd68f81d6a3da7
Remi Rampin <remirampin@gmail.com>
2013-09-14 10:45:16 -0400

    Imported from gist

---
+++ test.py
@@ -0,0 +1,32 @@
+import logstack
+import logging
+
+
+logger = logging.getLogger()
+
+
+format = logstack.Formatter("%(asctime)s %(levelname)s:\n%(message)s")
+console = logging.StreamHandler()
+console.setFormatter(format)
+logger.addHandler(console)
+
+
+def bar():
+    logstack.push(in_bar=True)
+
+def baz():
+    logstack.push(in_baz=True)
+    with logstack.pushed(something=[1, 2, 3]):
+        logger.warning("warning here")
+    raise ValueError("ohno")
+
+def foo():
+    logstack.push(in_foo=True)
+    bar()
+    logstack.push(out_of_bar=True)
+    baz()
+
+try:
+    foo()
+except:
+    logger.critical("Got an exception!", exc_info=True)
